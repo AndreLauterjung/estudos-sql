99c6c5b729203bfc2bf7aa9d43bbdcf79ddc13cf
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-31 18:20:58 -0300

    adiciona comando SQL select

--- 03-consultas.sql
+++ 03-consultas.sql
@@ -1,3 +1,5 @@
--- "SELECT *" seleciona todsa as counas e todas as linhas de uma tabela
-
+-- "SELECT *" seleciona todas as counas e todas as linhas de uma tabela
 SELECT * FROM usuario; -- Lê-se como: "mostre tudo da tabela usuario". --
+
+-- "SELECT" -- Selecionar apenas colunas específicas de uma tabela.
+SELECT nome FROM usuario;
