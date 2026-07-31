e464329d32ecff08c0cdc92a9517add5b3430d81
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-31 18:57:26 -0300

    Adiciona exemplo comando SQL SELECT AS

--- 03-consultas.sql
+++ 03-consultas.sql
@@ -3,3 +3,7 @@ SELECT * FROM usuario; -- Lê-se como: "mostre tudo da tabela usuario". --
 
 -- "SELECT" -- Selecionar apenas colunas específicas de uma tabela.
 SELECT nome FROM usuario;
+
+SELECT
+    nome AS "Coluna 1"
+FROM usuario;
