3d474ebcaaba08eca1c00ab406cd83cd3933f508
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-31 18:07:38 -0300

    atualiza comentario explicando o select

--- 03-consultas.sql
+++ 03-consultas.sql
@@ -1,2 +1,3 @@
--- Traz TODAS as colunas e TODOS os registros da tabela
+-- "SELECT *" seleciona todsa as counas e todas as linhas de uma tabela
+
 SELECT * FROM usuario; -- Lê-se como: "mostre tudo da tabela usuario". --
