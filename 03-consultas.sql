ff6967c0d20885a7b9db664e4c9f035cbe3b40a3
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-08-01 20:00:27 -0300

    feat(03-consultas): organiza visualmente os comandos sql

--- 03-consultas.sql
+++ 03-consultas.sql
@@ -1,12 +1,17 @@
 -- "SELECT *" seleciona todas as counas e todas as linhas de uma tabela
 SELECT * FROM usuario; -- Lê-se como: "mostre tudo da tabela usuario". --
 
+
+
 -- "SELECT" -- Seleciona apenas colunas específicas de uma tabela.
 SELECT nome FROM usuario;
+
 
 
 -- "SELECT AS" Seleciona colunas especíicas e dá um nome para elas.
 SELECT
     nome AS "Coluna 1"
 FROM usuario;
+
+
 
